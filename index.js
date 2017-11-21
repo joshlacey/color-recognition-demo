912e69066d964341b0bbd6c0c3d3c0449163ed80
joshlacey <williamjoshualacey@gmail.com>
2017-11-21 17:18:44 -0500

    glasses change slowly, needs work

--- index.js
+++ index.js
@@ -1,64 +1,56 @@
-// const express = require('express');
-// const app = express();
-//
-//
-// app.use('/static', express.static('static'));
-// app.all('*', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-//
-// app.listen(process.env.PORT || 3000);
+var video = document.getElementById('video'),
+    glasses = new Image(),
+    canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
 
-window.onload = function() {
-  var video = document.getElementById('video'),
-      // canvas = document.getElementById('canvas'),
-      // context = canvas.getContext('2d'),
-      tracker = new tracking.ColorTracker(['magenta']);
+var glassesOptions = ['img/glasses.png', 'img/ninties-dude.png', 'img/pointed-round.png'], totalCalls = 0;
+    currentOption = 0;
 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-  var positions = [0]
-  var timeNow = Date.now()
-
-  tracking.track('#video', tracker, { camera: true })
-
-  tracker.on('track', function(event) {
-    event.data.forEach(function(rect) {
-     if (rect.color === 'magenta') {
-        positions.push(rect.x)
-        console.log(positions[positions.length-1] - positions[0])
-      }
-    })
-  })
-
-  //add Animate function to swipe.
-
-  function swipeScreenRL() {
-    console.log("swiped Right to Left")
-    cube.rotation.y += 500
-    render()
-    console.log("rotation", cube.rotation.y)
-    positions = []
-  }
-
-  function swipeScreenLR() {
-    console.log("swiped Left to Right")
-    cube.rotation.y -= 500
-    render()
-    console.log("rotation", cube.rotation.y)
-
-    positions = []
-  }
-
-  (function loop() {
-    if(positions[positions.length-1] - positions[0] > 200) {
-      swipeScreenRL()
-    } else if(positions[positions.length-1] - positions[0] < -200) {
-      swipeScreenLR()
-    }
-    if ((Date.now() - timeNow) > 1000) {
-      positions = []
-      timeNow = Date.now()
-    }
-    requestAnimationFrame(loop)
-  })()
+if(navigator.getUserMedia) {
+  navigator.getUserMedia({video:true}, handleVideo, handleError)
 }
+
+function changeGlassesLeft() {
+  if (currentOption === 0) {
+    currentOption = glassesOptions.length - 1
+  } else {
+    --currentOption
+  }
+}
+
+function changeGlassesRight() {
+  if (currentOption === glassesOptions.length - 1) {
+    currentOption = 0
+  } else {
+    currentOption ++
+  }
+}
+
+function handleVideo(stream) {
+  video.src = window.URL.createObjectURL(stream)
+}
+
+function handleError(e) {
+    alert(e)
+}
+
+function html5glasses() {
+  glasses.src = glassesOptions[currentOption]
+  ctx.drawImage(video, 0, 0, video.width, video.height, 0, 0, canvas.width, canvas.height)
+  var comp = ccv.detect_objects({'canvas': (ccv.pre(canvas)), 'cascade': cascade, 'interval': 5, 'min_neighbors': 1})
+  for (var i = 0; i < comp.length; i++) {
+    totalCalls++
+    ctx.drawImage(glasses, comp[i].x, comp[i].y, comp[i].width, comp[i].height)
+  }
+  console.log('html5glasses called', glasses.src, glasses, totalCalls)
+}
+
+video.addEventListener('play', function() {
+    vidInterval = setInterval(html5glasses, 200)
+})
+
+video.addEventListener('ended', function() {
+    clearInterval(vidInterval)
+})
